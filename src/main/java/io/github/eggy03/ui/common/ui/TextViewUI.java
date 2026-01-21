d694049630a1372857e473a0a87c16bff38a2975
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-01-21 21:42:50 +0530

    refactor: clean up `TextViewUI` component setup
    - reorder imports in `TextViewUI.java` for better organization
    - simplify `JScrollPane` initialization by passing `JEditorPane` directly
    - adjust `GridLayout` parameters from `(1, 0, 0, 0)` to `(1, 1, 0, 0)`
    - remove redundant `EmptyBorder` from `contentPane`

--- src/main/java/io/github/eggy03/ui/common/ui/TextViewUI.java
+++ src/main/java/io/github/eggy03/ui/common/ui/TextViewUI.java
@@ -1,21 +1,22 @@
 package io.github.eggy03.ui.common.ui;
 
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.IOUtils;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.WindowConstants;
-import javax.swing.border.EmptyBorder;
-import java.awt.GridLayout;
-import java.awt.Toolkit;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class TextViewUI extends JFrame {
@@ -43,19 +44,16 @@ public class TextViewUI extends JFrame {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(TextViewUI.class.getResource("/icons/icon_main.png")));
 
 		JPanel contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new GridLayout(1, 0, 0, 0));
-
-		setContentPane(contentPane);
-
-		JScrollPane contentScrollPane = new JScrollPane();
-		contentPane.add(contentScrollPane);
+		contentPane.setLayout(new GridLayout(1, 1, 0, 0));
 
 		JEditorPane contentEditorPane = new JEditorPane();
-		contentScrollPane.setViewportView(contentEditorPane);
 		contentEditorPane.setEditable(false);
 		contentEditorPane.setContentType("text/plain");
 		contentEditorPane.setText(contents);
+		
+		contentPane.add(new JScrollPane(contentEditorPane));
+
+		setContentPane(contentPane);
 	}
 
 }
